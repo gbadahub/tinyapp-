4e9755f3ad20eceb9e04ef162fff658e010288a0
Georgina Bada <georginabada@gmail.com>
2021-12-06 18:13:33 -0500

    Filling Out the urls_index.ejs Template

--- express_server.js
+++ express_server.js
@@ -11,11 +11,17 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {

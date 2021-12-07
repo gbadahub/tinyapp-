19a6c7124085bd83885eabefc98f6b682e2afde1
Georgina Bada <georginabada@gmail.com>
2021-12-06 21:24:05 -0500

    Redirect After Form Submission and Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -8,51 +8,70 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 function generateRandomString() {
-  const bodyParser = require("body-parser");
-  app.use(bodyParser.urlencoded({extended: true}));
-
-  app.post("/urls", (req, res) => {
-    console.log(req.body);  // Log the POST request body to the console
-    res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  });
-  
-  app.get("/", (req, res) => {
-    res.send("Hello!");
-  });
-  
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-  
-  app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
-  });
-  
-  app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
-  });
-  
-  
-  app.get("/urls/:shortURL", (req, res) => {
-    //console.log(req.params)
-    console.log(req.query)
-    const templateVars =
-    
-     { shortURL: req.params.shortURL, 
-      
-      // urldatabases as pbject uses shortURL as key
-      longURL: urlDatabase[req.params.shortURL] };
-    res.render("urls_show", templateVars);
-  });
-  
-  
-  app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`);
-  });
+    let randomString = "";
+    for (let i = 0; i < 6; i++) {
+      const randomCharCode = Math.floor(Math.random() * 26 + 97);
+      const randomChar = String.fromCharCode(randomCharCode);
+      randomString += randomChar;
+    }
+    return randomString;
 }
+
+
+const bodyParser = require("body-parser");
+const { request } = require("express");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  
+  const templateVars =
+  
+   { shortURL: req.params.shortURL, 
+    
+    // urldatabases as pbject uses shortURL as key
+    longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL] 
+  res.redirect(longURL);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  const newShortURL = generateRandomString()
+  urlDatabase[newShortURL] = req.body.longURL
+  res.redirect("/urls")
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 
 

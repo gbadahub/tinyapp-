5394dcc85d2575e94fd6ef40fbeca40eb85f172f
Georgina Bada <georginabada@gmail.com>
2021-12-06 20:25:53 -0500

    Created URL and add a POST Route to Receive the Form Submission

--- express_server.js
+++ express_server.js
@@ -7,35 +7,52 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  //console.log(req.params)
-  console.log(req.query)
-  const templateVars =
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
   
-   { shortURL: req.params.shortURL, 
+  app.get("/", (req, res) => {
+    res.send("Hello!");
+  });
+  
+  app.get("/urls.json", (req, res) => {
+    res.json(urlDatabase);
+  });
+  app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+  });
+  
+  app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+  
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+  
+  
+  app.get("/urls/:shortURL", (req, res) => {
+    //console.log(req.params)
+    console.log(req.query)
+    const templateVars =
     
-    // urldatabases as pbject uses shortURL as key
-    longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
+     { shortURL: req.params.shortURL, 
+      
+      // urldatabases as pbject uses shortURL as key
+      longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
+  });
+  
+  
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
+}
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+

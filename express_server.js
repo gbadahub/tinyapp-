f73ed7fe8969f421c660abd5b50818e024f9b122
Georgina Bada <georginabada@gmail.com>
2021-12-07 17:15:25 -0500

    Updating URLs

--- express_server.js
+++ express_server.js
@@ -68,7 +68,14 @@ app.post("/urls", (req, res) => {
 app.post("/urls/:shortURL/delete", (req,res)=>{
 const shortURL = req.params.shortURL
 delete urlDatabase[shortURL]
+res.redirect("/urls")
 });
+
+app.post("/urls/:id", (req,res)=>{
+  const shortURL = req.params.id
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect("/urls")
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

6516339aebcda097019e5bf45922cb494fc767b8
Georgina Bada <georginabada@gmail.com>
2021-12-07 19:47:50 -0500

    Adding login and logout buttons

--- express_server.js
+++ express_server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-const bodyParser = require("body-parser");
+
+
 
 
 // hold database of preset urls includes shorturl and longurls
@@ -10,6 +11,10 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 // generates random string  for ShortURL
 function generateRandomString() {
@@ -21,7 +26,6 @@ function generateRandomString() {
     }
     return randomString;
 }
-
 
 
 const { request } = require("express");
@@ -42,7 +46,7 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };  
   res.render("urls_index", templateVars);
 });
 
@@ -84,9 +88,17 @@ app.post("/urls/:id", (req,res)=>{
   res.redirect("/urls")
   });
 
-app.post("urls/login", (req,res) =>{
+  app.post("/login", (req,res) =>{
+    const username = req.body.username;
+    res.cookie("username",username);
+    res.redirect("/urls");
+  });
 
-});
+  app.post("/logout", (req,res) =>{
+    const username = req.body.username;
+    res.clearCookie("username",username);
+    res.redirect("/urls");
+  });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
